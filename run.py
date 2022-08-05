7454e725b6e5196757ea8bf90e6c15b9e8d19485
hecoter <92445963+hecoter@users.noreply.github.com>
2022-08-06 00:45:59 +0800

    Delete run.py

--- run.py
+++
@@ -1,6 +0,0 @@
-import sys
-import subprocess
-import os
-from H_m3u8DL import m3u8download
-import m3u8
-m3u8download(m3u8url='https://hls.videocc.net/4adf37ccc0/a/4adf37ccc0342e919fef2de4d02b473a_3.m3u8',method='CHACHA',key='910d9a4a6c86d450d29b3a6a4eca3fd3',nonce='910d9a4a6c86d450d29b3a6a4eca3fd3')
