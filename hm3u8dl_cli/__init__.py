d6682b35d513002381ddf59ab109d5138ce4248b
hecoter <92445963+hecoter@users.noreply.github.com>
2022-12-07 20:26:34 +0800

    Delete hm3u8dl_cli directory

--- hm3u8dl_cli/__init__.py
+++
@@ -1,6 +0,0 @@
-from .m3u8download import m3u8download
-from .util import Util,M3U8InfoObj
-from .tsInfo import tsInfo
-from .idm import download as idm_download
-from .merge import Merge
-from .version import version
